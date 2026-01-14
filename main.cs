4e4435083b17f6e4141a0a2ac469adb807d7d9ce
Penguin Interactive <69770294+PengInt@users.noreply.github.com>
2026-01-14 23:52:14 +0000

    stuff

--- main.cs
+++ main.cs
@@ -1,7 +1,25 @@
 using System;
 
+
+class Vector3 {
+	public int X;
+	public int Y;
+	public int Z;
+
+	public void Vector3 (int x, int y, int z) {
+		X = x;
+		Y = y;
+		Z = z;
+	}
+}
+
+public static Vector3 operator + (Vector3 v1, Vector3 v2) {
+	Vector3 v3 = new Vector3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+	return v3;
+}
+
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+	public static void Main (string[] args) {
+
+	}
 }
