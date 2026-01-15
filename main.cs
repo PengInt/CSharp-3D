e9c74ba43f2a3360c0cee764f5a5e51077696a6d
Penguin Interactive <69770294+PengInt@users.noreply.github.com>
2026-01-15 18:35:43 +0000

    V3 Chennanigans

--- main.cs
+++ main.cs
@@ -1,7 +1,7 @@
 using System;
 
 
-class Vector3 {
+struct Vector3 {
 	public int X;
 	public int Y;
 	public int Z;
@@ -11,12 +11,41 @@ class Vector3 {
 		Y = y;
 		Z = z;
 	}
+
+	public static Vector3 operator + (Vector3 v1, Vector3 v2) {
+		Vector3 v3 = new Vector3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+		return v3;
+	}
+
+	public static Vector3 operator * (Vector3 v1, Vector3 v2) {
+		//Ay*Bz - Az*By
+		Vector3 v3 = new Vector3(
+			v1.Y * v2.Z - v1.Z * v2.Y,
+			v1.X * v2.Z - v1.Z * v2.X,
+			v1.X * v2.Y - v1.Y * v2.X
+		);
+		return v3;
+	}
+
+	public static Vector3 operator % (Vector3 v1, Vector3 v2) {
+		//Ay*Bz - Az*By
+		Vector3 v3 = new Vector3(
+			v1.X * v2.X,
+			v1.Y * v2.Y,
+			v1.Z * v2.Z
+		);
+		return v3;
+	}
 }
 
-/*public static Vector3 operator + (Vector3 v1, Vector3 v2) {
-	Vector3 v3 = new Vector3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
-	return v3;
-}*/
+class Vertex {
+	public Vector3 Vector3;
+
+	public Vertex (Vector3 vector3) {
+		Vector3 = vector3;
+	}
+}
+
 
 class Program {
 	public static void Main (string[] args) {
