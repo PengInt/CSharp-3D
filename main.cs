5df32aaef608f406f736f32cc0cff21dd8663f7c
PengInt <troy.gomme@gmail.com>
2026-01-14 20:42:06 -0500

    Fixes

--- main.cs
+++ main.cs
@@ -6,17 +6,17 @@ class Vector3 {
 	public int Y;
 	public int Z;
 
-	public void Vector3 (int x, int y, int z) {
+	public Vector3 (int x, int y, int z) {
 		X = x;
 		Y = y;
 		Z = z;
 	}
 }
 
-public static Vector3 operator + (Vector3 v1, Vector3 v2) {
+/*public static Vector3 operator + (Vector3 v1, Vector3 v2) {
 	Vector3 v3 = new Vector3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
 	return v3;
-}
+}*/
 
 class Program {
 	public static void Main (string[] args) {
